232b71fef51cd1cdf1e81be25bc6f3901f581e2a
arbaz52 <arbaz5256@gmail.com>
2020-09-24 13:13:12 +0500

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07d99080044009b20ade8442da0bdbac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fc7a4b3a-fb0b-4e90-8313-71275fc98214'
+};
